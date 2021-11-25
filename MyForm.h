aae6af7d15cd79fa5cf00743e8adc514ff277d44
Talla DIOP <talla.diop@viacesi.fr>
2021-11-25 22:14:27 +0100

    Couleur bouton valider

--- MyForm.h
+++ MyForm.h
@@ -88,6 +88,7 @@ namespace projectView {
 			// 
 			// boutonValider
 			// 
+			this->boutonValider->BackColor = System::Drawing::SystemColors::ActiveCaption;
 			this->boutonValider->Location = System::Drawing::Point(15, 48);
 			this->boutonValider->Margin = System::Windows::Forms::Padding(4);
 			this->boutonValider->Name = L"boutonValider";
