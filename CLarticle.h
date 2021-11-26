a5adbb25a53ced5393a859aef4aac3a2142c9139
Talla DIOP <talla.diop@viacesi.fr>
2021-11-26 10:40:02 +0100

    introduction classe Article

---
+++ CLarticle.h
@@ -0,0 +1,33 @@
+#pragma once
+
+ref class CLarticle
+{
+private:
+
+	int ref_article, stock_article, quantite_reapprovisionnement;
+	System::String^ nature;
+	System::String^ nom_article;
+	System::String^ couleur_article;
+	float remise_article, prix_article_ht, taux_tva;
+
+public:
+
+    void setNature(System::String^);
+	void setNom(System::String^);
+	void setStock(int);
+	void setRemise(float);
+	void setPrixHT(float);
+	void setQuantiteReapprovisionnement(int);
+	void setTauxTVA(float);
+	void setCouleur(System::String^);
+	int getReference();
+	System::String^ getNom();
+	int getStock();
+	float getRemise();
+	float getPrixHT();
+	int getQuantiteReapprovisionnement();
+	float getTauxTVA();
+	System::String^ getCouleur();
+
+};
+
