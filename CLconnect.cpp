2cc1ea3067472d121cdcae2c14a510ce1d2d864a
Good660 <good660.minecraft@gmail.com>
2021-11-26 10:31:59 +0100

    Màj nom variable CAD pour correspondre au diagramme de classe

--- CLconnect.cpp
+++ CLconnect.cpp
@@ -2,34 +2,34 @@
 #include "CLconnect.h"
 
 NS_Comp_Data::CLcad::CLcad(void) {
-	this->sCnx = "Data Source=sql5102.site4now.net;Initial Catalog=db_a7ccf4_rouencaen001;Persist Security Info=True;User ID=db_a7ccf4_rouencaen001_admin;Password=TTSF7676";
+	this->connexion_s = "Data Source=sql5102.site4now.net;Initial Catalog=db_a7ccf4_rouencaen001;Persist Security Info=True;User ID=db_a7ccf4_rouencaen001_admin;Password=TTSF7676";
 
-	this->sSql = "undefined";
+	this->requete_SQL = "undefined";
 
-	this->oCnx = gcnew System::Data::SqlClient::SqlConnection(this->sCnx);
-	this->oCmd = gcnew System::Data::SqlClient::SqlCommand(this->sSql, this->oCnx);
-	this->oDa = gcnew System::Data::SqlClient::SqlDataAdapter();
-	this->oDs = gcnew System::Data::DataSet();
+	this->SqlConnection = gcnew System::Data::SqlClient::SqlConnection(this->connexion_s);
+	this->SqlCommand = gcnew System::Data::SqlClient::SqlCommand(this->requete_SQL, this->SqlConnection);
+	this->SqlDataAdapter = gcnew System::Data::SqlClient::SqlDataAdapter();
+	this->DataSet = gcnew System::Data::DataSet();
 
-	this->oCmd->CommandType = System::Data::CommandType::Text;
+	this->SqlCommand->CommandType = System::Data::CommandType::Text;
 
 }
 
 System::Data::DataSet^ NS_Comp_Data::CLcad::getRows(System::String^ sSql, System::String^ sDataTableName) {
-	this->oDs->Clear();
-	this->sSql = sSql;
-	this->oCmd->CommandText = this->sSql;
-	this->oDa->SelectCommand = this->oCmd;
-	this->oDa->Fill(this->oDs, sDataTableName);
+	this->DataSet->Clear();
+	this->requete_SQL = sSql;
+	this->SqlCommand->CommandText = this->requete_SQL;
+	this->SqlDataAdapter->SelectCommand = this->SqlCommand;
+	this->SqlDataAdapter->Fill(this->DataSet, sDataTableName);
 
-	return this->oDs;
+	return this->DataSet;
 }
 
 void NS_Comp_Data::CLcad::actionRows(System::String^ sSql) {
-	this->sSql = sSql;
-	this->oCmd->CommandText = this->sSql;
-	this->oDa->SelectCommand = this->oCmd;
-	this->oCnx->Open();
-	this->oCmd->ExecuteNonQuery();
-	this->oCnx->Close();
+	this->requete_SQL = sSql;
+	this->SqlCommand->CommandText = this->requete_SQL;
+	this->SqlDataAdapter->SelectCommand = this->SqlCommand;
+	this->SqlConnection->Open();
+	this->SqlCommand->ExecuteNonQuery();
+	this->SqlConnection->Close();
 }
